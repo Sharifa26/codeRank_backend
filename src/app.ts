e259d49da499192d9774b1a44be075f352e0c95c
Sharifa26 <ismailsheriff2001@gmail.com>
2026-05-07 21:59:05 +0530

    Adding url

--- src/app.ts
+++ src/app.ts
@@ -80,7 +80,7 @@ app.use((req, res, next) => {
 // CORS configuration
 app.use(
   cors({
-    origin: env.FRONTEND_URL,
+    origin: [env.FRONTEND_URL, env.BACKEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
